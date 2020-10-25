9af89b13b8946287b3eebfb432bb8e5dad236509
danymessi1000@gmail.com <danymessi1000@gmail.com>
2020-10-24 23:41:30 -0500

    Agregue las listas y variables

---
+++ Codigo.py
@@ -0,0 +1,54 @@
+#Listas de los cargos de cada cuenta
+Cargos1000 = [0]
+Cargos1001 = [0]
+Cargos1002 = [0]
+Cargos1003 = [0]
+Cargos1004 = [0]
+Cargos1005 = [0]
+Cargos1006 = [0]
+Cargos1007 = [0]
+Cargos1008 = [0]
+Cargos1009 = [0]
+#Listas de abonos de las cuentas
+Abonos1000 = [0]
+Abonos1001 = [0]
+Abonos1002 = [0]
+Abonos1003 = [0]
+Abonos1004 = [0]
+Abonos1005 = [0]
+Abonos1006 = [0]
+Abonos1007 = [0]
+Abonos1008 = [0]
+Abonos1009 = [0]
+#Suma de cada listas
+BanCargos=0
+BanAbonos=0
+ProvCargos=0
+ProvAbonos=0
+TerrCargos=0
+TerrAbonos=0
+InvCargos=0
+InvAbonos=0
+EdiCargos=0
+EdiAbonos=0
+EqcomCargos=0
+EqcomAbonos=0
+EqtransCargos=0
+EqtransAbonos=0
+CapCargos=0
+CapAbonos=0
+GastoCargos=0
+GastoAbonos=0
+CostoCargos=0
+CostoAbonos=0
+#Total de cada cuenta
+BancoTot=0
+ProvTot=0
+TerrTot=0
+InvTot=0
+EdiTot=0
+EqcomTot=0
+EqtransTot=0
+CapTot=0
+GastoTot=0
+CostoTot=0
